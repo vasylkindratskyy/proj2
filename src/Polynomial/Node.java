a01c652d7c12dc06ccbb692851d4b7ef8f2d99d1
Vasyl Kindratskyy <vasylkindratskyy@gmail.com>
2023-03-08 13:25:41 -0500

    changed node variable to nextNode

--- src/Polynomial/Node.java
+++ src/Polynomial/Node.java
@@ -4,31 +4,31 @@ public class Node {
 //  Initialize Variables-------------------------------------------------
     private int exponent;
     int coefficient;
-    private Node node;
+    private Node nextNode;
 
 //  Constructors---------------------------------------------------------
     public Node(int exponent, int coefficient){
         this.exponent = exponent;
         this.coefficient = coefficient;
-        this.node = null;
+        this.nextNode = null;
     }
 
-    public Node(int exponent, int coefficient, Node node){
+    public Node(int exponent, int coefficient, Node nextNode){
         this.exponent = exponent;
         this.coefficient = coefficient;
-        this.node = node;
+        this.nextNode = nextNode;
     }
 // Setters-----------------------------------------------------------
     public void setExponent (int exponent){this.exponent = exponent;}
 
     public void setCoefficient (int coefficient){this.coefficient = coefficient;}
 
-    public void setNode (Node node){this.node = node;}
+    public void setNextNode (Node nextNode){this.nextNode = nextNode;}
 
 // Getters-----------------------------------------------------------
     public int getExponent (){return this.exponent;}
 
     public int getCoefficient (){return this.coefficient;}
 
-    public Node getNode (){return this.node;}
+    public Node getNode (){return this.nextNode;}
 }
--- src/Polynomial/Polynomial.java
+++ src/Polynomial/Polynomial.java
@@ -1,5 +1,4 @@
 package Polynomial;
 
 public class Polynomial {
-
 }
