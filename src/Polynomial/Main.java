8d4a1af485cc49902febeea411bb4aeba6fb4bb6
Vasyl Kindratskyy <vasylkindratskyy@gmail.com>
2023-03-13 08:20:21 -0400

    Polynomial class and Main class finished, ready for debugg

--- src/Polynomial/Main.java
+++ src/Polynomial/Main.java
@@ -1,7 +1,56 @@
 package Polynomial;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList<Polynomial> polynomialList = new ArrayList<>();
+
+        try {
+            File file = new File("polynomials.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String polynomialString = scanner.nextLine();
+                Polynomial polynomial = new Polynomial(polynomialString);
+                polynomialList.add(polynomial);
+                System.out.println(polynomialList.indexOf(polynomial) + ": " + polynomial);
+            }
+            scanner.close();
+        }catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int polyIndex1, polyIndex2;
+        while (true) {
+            System.out.println("Which polynomials do you wish to add? Press -1 to exit.");
+            try {
+                String input = scanner.nextLine();
+                if (input.equals("-1")) {
+                    break;
+                }
+                String[] inputArray = input.split("  ");
+                if (inputArray.length != 2) {
+                    System.out.println("Invalid input.");
+                    continue;
+                }
+                polyIndex1 = Integer.parseInt(inputArray[0]);
+                polyIndex2 = Integer.parseInt(inputArray[0]);
+                if (polyIndex1 <  0 || polyIndex1 >= polynomialList.size() || polyIndex2 < 0 || polyIndex2 >= polynomialList.size()){
+                    System.out.println("Invalid input.");
+                    continue;
+                }
+                Polynomial result = Polynomial.add(polynomialList.get(polyIndex1), polynomialList.get(polyIndex2));
+                polynomialList.add(result);
+                System.out.println(polynomialList.indexOf(result) + ": " + result);
+            }catch (NumberFormatException e) {
+                System.out.println("Invalid input.");
+            }
+        }
+
+        scanner.close();
     }
 }
--- src/Polynomial/Polynomial.java
+++ src/Polynomial/Polynomial.java
@@ -1,5 +1,6 @@
 package Polynomial;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Polynomial {
@@ -51,5 +52,89 @@ public class Polynomial {
                }
            }
        }
+    }
+
+    public Polynomial(Polynomial otherPoly) {
+        headNode = null;
+        Node currentNode = otherPoly.headNode;
+        while (currentNode != null) {
+            Node tempNode = new Node(currentNode.getExponent(), currentNode.getCoefficient());
+            if (headNode == null) {
+                headNode = tempNode;
+            } else {
+                Node lastNode = headNode;
+                while (lastNode.getNextNode() != null) {
+                    lastNode = lastNode.getNextNode();
+                }
+                lastNode.setNextNode(tempNode);
+            }
+            currentNode = currentNode.getNextNode();
+        }
+    }
+
+    public void print() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Node currentNode = headNode;
+        while (currentNode != null) {
+            if (currentNode.getCoefficient() != 0 ){
+                if (currentNode.getExponent() == 0) {
+                   stringBuilder.append(currentNode.getCoefficient());
+                } else if (currentNode.getExponent() == 1) {
+                    if (currentNode.getCoefficient() == 1) {
+                        stringBuilder.append("x");
+                    } else if (currentNode.getCoefficient() == -1) {
+                        stringBuilder.append("-x");
+                    }  else {
+                       stringBuilder.append(currentNode.getCoefficient()).append("x");
+                    }
+                } else {
+                    if (currentNode.getCoefficient() == 1) {
+                        stringBuilder.append("x^").append(currentNode.getExponent());
+                    } else if (currentNode.getCoefficient() == -1) {
+                        stringBuilder.append("-x^").append(currentNode.getExponent());
+                    } else {
+                        stringBuilder.append(currentNode.getCoefficient()).append("x^").append(currentNode.getExponent());
+                    }
+                }
+                if (currentNode.getNextNode() != null && currentNode.getNextNode().getCoefficient() > 0) {
+                    stringBuilder.append("+");
+                }
+            }
+            currentNode = currentNode.getNextNode();
+        }
+        System.out.println(stringBuilder.toString());
+    }
+    public static Polynomial add(Polynomial poly1, Polynomial poly2) {
+        Node current1 = poly1.headNode;
+        Node current2 = poly2.headNode;
+        Polynomial result = new Polynomial();
+        Node current3 = null;
+        while (current1 != null || current2 != null) {
+            Node node;
+            if (current1 == null){
+                node = new Node(current2.getExponent(), current2.getCoefficient());
+                current2 = current2.getNextNode();
+            } else if (current2 == null) {
+                node = new Node(current1.getExponent(), current1.getCoefficient());
+                current1 = current1.getNextNode();
+            } else if (current1.getExponent() > current2.getExponent()) {
+                node = new Node(current1.getExponent(), current1.getCoefficient());
+                current1 = current1.getNextNode();
+            } else if (current2.getExponent() > current1.getExponent()) {
+                node = new Node(current2.getExponent(), current2.getCoefficient());
+                current2 = current2.getNextNode();
+            } else {
+                node = new Node(current1.getExponent(), current1.getCoefficient() + current2.getCoefficient());
+                current1 = current1.getNextNode();
+                current2 = current2.getNextNode();
+            }
+            if (current3 == null) {
+                result.headNode = node;
+            }else {
+                current3.setNextNode(node);
+            }
+            current3 = node;
+        }
+        return result;
     }
 }
